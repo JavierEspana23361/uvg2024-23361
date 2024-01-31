e336a4b09b7443ca07fe727137478d2df67c76a0
AndreEstrada1x <est23659@uvg.edu.gt>
2024-01-30 18:15:58 -0600

    Stack

--- HDT2/src/App.java
+++ HDT2/src/App.java
@@ -1,5 +1,34 @@
+import java.util.Stack;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // Crear una instancia de Stack
+        Stack<Integer> stack = new Stack<>();
+
+        // Agregar elementos a la pila
+        stack.push(2);
+        stack.push(5);
+        stack.push(8);
+
+        // Imprimir el elemento en la cima de la pila
+        System.out.println("Elemento en la cima de la pila: " + stack.peek());
+
+        // Imprimir la cantidad de elementos en la pila
+        System.out.println("Cantidad de elementos en la pila: " + stack.size());
+
+        // Imprimir si la pila está vacía
+        System.out.println("¿La pila está vacía? " + stack.isEmpty());
+
+        // Realizar operación pop para obtener y eliminar el elemento en la cima de la pila
+        int poppedElement = stack.pop();
+        System.out.println("Elemento obtenido mediante pop: " + poppedElement);
+
+        // Imprimir la pila después de la operación pop
+        System.out.println("Pila después de pop: " + stack);
+
+        // Imprimir la cantidad de elementos en la pila después de la operación pop
+        System.out.println("Cantidad de elementos en la pila: " + stack.size());
     }
 }
+
+
--- HDT2/src/Stack.java
+++ HDT2/src/Stack.java
@@ -1,3 +1,43 @@
-public class Stack {
-    
+import java.util.ArrayList;
+
+public class Stack<T> implements IStack<T> {
+
+    private ArrayList<T> stack;
+
+    public Stack() {
+        stack = new ArrayList<>();
+    }
+
+    @Override
+    public int count() {
+        return stack.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return stack.isEmpty();
+    }
+
+    @Override
+    public void push(T value) {
+        stack.add(value);
+    }
+
+    @Override
+    public T pop() {
+        if (!isEmpty()) {
+            return stack.remove(stack.size() - 1);
+        } else {
+            throw new IllegalStateException("ERROR: Stack Vacio");
+        }
+    }
+
+    @Override
+    public T peek() {
+        if (!isEmpty()) {
+            return stack.get(stack.size() - 1);
+        } else {
+            throw new IllegalStateException("ERROR: Stack Vacio");
+        }
+    }
 }
