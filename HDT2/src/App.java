15decf1c8bfa92f264cd1540aa7ca571a9fb7f1c
AngelEsquit <esq23221@uvg.edu.gt>
2024-01-30 21:15:47 -0600

    Corrección mostrar resultado

--- HDT2/src/App.java
+++ HDT2/src/App.java
@@ -2,7 +2,7 @@ public class App {
     public static void main(String[] args) {
         Calculator calc = new Calculator();
         try {
-            System.out.println(calc.solve(calc.read("datos.txt")));
+            calc.solve(calc.read("datos.txt"));
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
