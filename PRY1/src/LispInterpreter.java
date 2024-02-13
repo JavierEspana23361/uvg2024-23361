e7410a4d8f99092440f6e09056e4baa6a4499772
AngelEsquit <esq23221@uvg.edu.gt>
2024-02-12 20:53:39 -0600

    Quote

--- PRY1/src/LispInterpreter.java
+++ PRY1/src/LispInterpreter.java
@@ -16,8 +16,8 @@ public class LispInterpreter {
         return n1 / n2;
 
     }
-   public int QUOTE(int n1, int n2){
-        
+    public int QUOTE(int n1, int n2){
+        return n1;
     }
      
 }
