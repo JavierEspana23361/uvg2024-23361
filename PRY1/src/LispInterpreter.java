9c1a0177e1f75ba225e9a192ebe410482d566b7d
JavierEspana23361 <esp23361@uvg.edu.gt>
2024-02-12 21:20:52 -0600

    ...

--- PRY1/src/LispInterpreter.java
+++ PRY1/src/LispInterpreter.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class LispInterpreter {
     public int add(int n1, int n2){
 
@@ -34,17 +36,67 @@ public class LispInterpreter {
     public int EQUAL(int n1, int n2){
         return n1;
     }
-    public int LESS(int n1, int n2){
-        return n1;
+    public String LESS(int n1, int n2){
+        String X = "True";
+        String Y = "False";
+        if (n1 < n2){
+        
+            return X;
+        }
+        else{
+            return Y;
+        }
     }
-    public int GREATER(int n1, int n2){
-        return n1;
+    public String GREATER(int n1, int n2){
+        String X = "True";
+        String Y = "False";
+        if (n1 > n2){
+        
+            return X;
+        }
+        else{
+            return Y;
     }
     public int COND(int n1, int n2){
         return n1;
     }
 
-    
+    public ArrayList<String> {
+            
+        ArrayList<String> elements = new ArrayList<>();
+        //Añadir el lector de LISP
+        
+           
+        return elements;
+    }
+
+    public int solve(ArrayList<String> elements) throws Exception {
+        Stack<Integer> stack = new Stack<>();
+
+        for (String element : elements) {
+            if (isOperand(element)) {
+                stack.push(Integer.parseInt(element));
+            } else if (isOperator(element)) {
+                if (stack.count() < 2) {
+                    throw new IllegalArgumentException("Error: Insufficient operands");
+                }
+                int n2 = stack.pop();
+                int n1 = stack.pop();
+                int result = performOperation(n1, n2, element);
+                stack.push(result);
+            }
+        }
+
+        if (stack.count() != 1) {
+            throw new IllegalArgumentException("Error: Invalid expression");
+        }
+
+        int finalResult = stack.pop();
+        System.out.println("Resultado: " + finalResult);
+        return finalResult;
+    }
+
+
     private boolean isOperand(String element) {
         try {
             Integer.parseInt(element);
@@ -58,6 +110,39 @@ public class LispInterpreter {
         return element.equals("+") || element.equals("-") || element.equals("*") || element.equals("/") || 
         element.equals("QUOTE") || element.equals("DEFUN") || element.equals("SETQ") || element.equals("ATOM") || element.equals("LIST")
         || element.equals("EQUAL") || element.equals("<") || element.equals(">") || element.equals("COND");
+    }
+    private int performOperation(int n1, int n2, String operator) {
+        switch (operator) {
+            case "+":
+                return add(n1, n2);
+            case "-":
+                return substract(n1, n2);
+            case "*":
+                return multiplication(n1, n2);
+            case "/":
+                return division(n1, n2);
+            case "QUOTE":
+                return QUOTE(n1);
+            case "DEFUN":
+                return DEFUN(n1, n2);
+            case "SETQ":
+                return SETQ(n1, n2);
+            case "ATOM":
+                return ATOM(n1);
+            case "LIST":   
+                return LIST(n1);
+            case "EQUAL":
+                return EQUAL(n1, n2);
+            case "<":
+                return LESS(n1, n2);
+            case ">":
+                return GREATER(n1, n2);
+            case "COND":
+                return COND(n1, n2);
+            
+            default:
+                throw new IllegalArgumentException("Error: Invalid operator");
+        }
     }
 }
      
