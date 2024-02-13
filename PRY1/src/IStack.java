ba3458d0fa36d40e42e8296019386d6022df4f9a
JavierEspana23361 <esp23361@uvg.edu.gt>
2024-02-12 20:48:43 -0600

    push

---
+++ PRY1/src/IStack.java
@@ -0,0 +1,15 @@
+
+public interface IStack<T>{
+
+    int count();
+    
+    boolean isEmpty();
+    
+    void push(T value);
+    
+    T pop();
+    
+    T peek();
+    
+}
+
---
+++ PRY1/src/LispInterpreter.java
@@ -0,0 +1,23 @@
+public class LispInterpreter {
+    public int add(int n1, int n2){
+
+        return n1 + n2;
+    }
+
+    public int substract(int n1, int n2){
+        return n1 - n2;
+
+    }
+    public int multiplication(int n1, int n2){
+        return n1 * n2;
+
+    }
+    public int division(int n1, int n2){
+        return n1 / n2;
+
+    }
+   public int QUOTE(int n1, int n2){
+        
+    }
+     
+}
