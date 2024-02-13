b11504ab850a8a3b7aeff63ff1e228d13a42cac8
JavierEspana23361 <esp23361@uvg.edu.gt>
2024-02-12 21:12:23 -0600

    plantilla

--- PRY1/src/LispInterpreter.java
+++ PRY1/src/LispInterpreter.java
@@ -19,5 +19,45 @@ public class LispInterpreter {
     public int QUOTE(int n1){
         return n1;
     }
-     
+    public int DEFUN(int n1, int n2){
+        return n1;
+    }
+    public int SETQ(int n1, int n2){
+        return n1;
+    }
+    public int ATOM(int n1){
+        return n1;
+    }
+    public int LIST(int n1){
+        return n1;
+    }
+    public int EQUAL(int n1, int n2){
+        return n1;
+    }
+    public int LESS(int n1, int n2){
+        return n1;
+    }
+    public int GREATER(int n1, int n2){
+        return n1;
+    }
+    public int COND(int n1, int n2){
+        return n1;
+    }
+
+    
+    private boolean isOperand(String element) {
+        try {
+            Integer.parseInt(element);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    private boolean isOperator(String element) {
+        return element.equals("+") || element.equals("-") || element.equals("*") || element.equals("/") || 
+        element.equals("QUOTE") || element.equals("DEFUN") || element.equals("SETQ") || element.equals("ATOM") || element.equals("LIST")
+        || element.equals("EQUAL") || element.equals("<") || element.equals(">") || element.equals("COND");
+    }
 }
+     
