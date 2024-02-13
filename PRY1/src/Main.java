13cfb9e0b7d81d987249bf2bacfbb92ea87f04d1
AngelEsquit <esq23221@uvg.edu.gt>
2024-02-12 21:57:54 -0600

    Update Main.java

--- PRY1/src/Main.java
+++ PRY1/src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
     
     public static void main(String[] args) {
-        
+        int f = 180;
+
+        LispInterpreter interpreter = new LispInterpreter();
+
+                // Convertir grados Fahrenheit a grados Celsius
+                double c = (f - 32) * 5 / 9;
+                System.out.println(f + " degrees Fahrenheit is equal to " + c + " degrees Celsius.");
     }
 }
