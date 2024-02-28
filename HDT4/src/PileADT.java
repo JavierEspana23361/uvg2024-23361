cc3fabbb301e74ff5d7e0165569a3e7a297bbf97
AngelEsquit <esq23221@uvg.edu.gt>
2024-02-27 20:01:38 -0600

    Update PileADT.java

--- HDT4/src/PileADT.java
+++ HDT4/src/PileADT.java
@@ -1,3 +1,41 @@
-public class PileADT implements IPileADT {
-    
+import java.util.ArrayList;
+import java.util.EmptyStackException;
+
+public class PileADT<E> implements IPileADT<E> {
+    protected ArrayList<E> elementos;
+
+    public PileADT() {
+        elementos = new ArrayList<>();
+    }
+
+    @Override
+    public void push(E elemento) {
+        elementos.add(elemento);
+    }
+
+    @Override
+    public E pop() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return elementos.remove(elementos.size() - 1);
+    }
+
+    @Override
+    public E peek() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return elementos.get(elementos.size() - 1);
+    }
+
+    @Override
+    public int size() {
+        return elementos.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return elementos.isEmpty();
+    }
 }
