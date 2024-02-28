86da63652251740e89c50acc0fd03c2b939aef60
AngelEsquit <esq23221@uvg.edu.gt>
2024-02-27 20:04:07 -0600

    Update ListADT.java

--- HDT4/src/ListADT.java
+++ HDT4/src/ListADT.java
@@ -1,3 +1,34 @@
-public class ListADT implements IListADT{
-    
+import java.util.ArrayList;
+
+public class ListADT<E> implements IListADT<E> {
+    protected ArrayList<E> elementos;
+
+    public ListADT() {
+        elementos = new ArrayList<>();
+    }
+
+    @Override
+    public void agregar(E elemento) {
+        elementos.add(elemento);
+    }
+
+    @Override
+    public E obtener(int indice) {
+        return elementos.get(indice);
+    }
+
+    @Override
+    public void eliminar(int indice) {
+        elementos.remove(indice);
+    }
+
+    @Override
+    public int size() {
+        return elementos.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return elementos.isEmpty();
+    }
 }
