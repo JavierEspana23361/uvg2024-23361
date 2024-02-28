b9da72fbbf5617c98bdf2e0f16f3c6dbe9bc54fc
JavierEspana23361 <esp23361@uvg.edu.gt>
2024-02-27 20:10:32 -0600

    aaaaaaaaaa

--- HDT4/src/infixRead.java
+++ HDT4/src/infixRead.java
@@ -1,3 +1,61 @@
-public class infixRead {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Stack;
+
+public class infixRead implements ICalculator{
     
+    public String read(String file) throws Exception{
+        StringBuilder infix = new StringBuilder();
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            infix.append(line);
+        }
+        reader.close();
+        return infix.toString();
+    }
+
+    public String infixToPostfix(String infix) {
+        StringBuilder postfix = new StringBuilder();
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < infix.length(); i++) {
+            char c = infix.charAt(i);
+
+            if (isOperand(c)) {
+                postfix.append(c).append(" ");
+            } else if (isOperator(c)) {
+                while (!stack.isEmpty() && stack.peek() != '(' && hasHigherPrecedence(stack.peek(), c)) {
+                    postfix.append(stack.pop()).append(" ");
+                }
+                stack.push(c);
+            } else if (c == '(') {
+                stack.push(c);
+            } else if (c == ')') {
+                while (!stack.isEmpty() && stack.peek() != '(') {
+                    postfix.append(stack.pop()).append(" ");
+                }
+                stack.pop(); 
+            }
+        }
+
+        while (!stack.isEmpty()) {
+            postfix.append(stack.pop()).append(" ");
+        }
+
+        return postfix.toString().trim();
+    }
+
+    private boolean isOperand(char c) {
+        return Character.isLetterOrDigit(c);
+    }
+
+    private boolean isOperator(char c) {
+        return c == '+' || c == '-' || c == '*' || c == '/';
+    }
+
+    private boolean hasHigherPrecedence(char op1, char op2) {
+        return (op1 == '*' || op1 == '/') && (op2 == '+' || op2 == '-');
+    }
+
 }
--- HDT4/src/postfixCalc.java
+++ HDT4/src/postfixCalc.java
@@ -1,3 +1,100 @@
-public class postfixCalc {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class postfixCalc implements ICalculator {
+    infixRead infix = new infixRead();
+
+    public int add(int n1, int n2){
+
+        return n1 + n2;
+    }
+
+    public int substract(int n1, int n2){
+        return n1 - n2;
+
+    }
     
+    public int multiplication(int n1, int n2){
+        return n1 * n2;
+
+    }
+    
+    public int division(int n1, int n2){
+        return n1 / n2;
+
+    }
+    
+    public int residue(int n1, int n2){
+        return n1 % n2;
+
+    }
+    
+    public static List<String> postfixreader(String pstfxexpression) {
+        ListADT list = new ListADT();
+        List<String> elements = new ArrayList<>();
+        String[] words = pstfxexpression.split(" ");
+        for (String word : words) {
+            elements.add(word);
+        }
+        return elements;
+    }
+
+    private boolean isOperand(String element) {
+        try {
+            Integer.parseInt(element);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    private boolean isOperator(String element) {
+        return element.equals("+") || element.equals("-") || element.equals("*") || element.equals("/") || element.equals("%");
+    }
+
+    private int performOperation(int n1, int n2, String operator) {
+        switch (operator) {
+            case "+":
+                return add(n1, n2);
+            case "-":
+                return substract(n1, n2);
+            case "*":
+                return multiplication(n1, n2);
+            case "/":
+                return division(n1, n2);
+            case "%":
+                return residue(n1, n2);
+            default:
+                throw new IllegalArgumentException("Error: Invalid operator");
+        }
+    }
+
+
+    
+    public int solve(ArrayList<String> elements) throws Exception {
+        Stack<Integer> stack = new Stack<>();
+
+        for (String element : elements) {
+            if (isOperand(element)) {
+                stack.push(Integer.parseInt(element));
+            } else if (isOperator(element)) {
+                if (stack.count() < 2) {
+                    throw new IllegalArgumentException("Error: Insufficient operands");
+                }
+                int n2 = stack.pop();
+                int n1 = stack.pop();
+                int result = performOperation(n1, n2, element);
+                stack.push(result);
+            }
+        }
+
+        if (stack.count() != 1) {
+            throw new IllegalArgumentException("Error: Invalid expression");
+        }
+
+        int finalResult = stack.pop();
+        System.out.println("Resultado: " + finalResult);
+        return finalResult;
+    }
 }
