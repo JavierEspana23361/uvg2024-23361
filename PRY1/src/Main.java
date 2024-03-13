73e6740d192c4c7fce81cbada16822ffb9a2087c
AngelEsquit <esq23221@uvg.edu.gt>
2024-03-12 22:15:49 -0600

    Pruebas COND

--- PRY1/src/Main.java
+++ PRY1/src/Main.java
@@ -14,15 +14,19 @@ public class Main {
         // Esta es la base para sacar la sucesión de fibonacci, n es el número buscado en suceción
         //String fibonacci = "(* (/ 1 (ROOT 5 2)) (- (EXP (/ (+ 1 (ROOT 5 2)) 2) n ) (EXP (/ (- 1 (ROOT 5 2)) 2) n ) ))";
         String fibonacci = "(+ 4 (* 3 2 1) 7)";
+
+        String condString = "(COND (> 4 5) (QUOTE hola) (QUOTE adios))";
       
         try {
             // Tokeniza la expresión antes de evaluarla
+            ArrayList<String> condtokens = lisp.tokenize(condString);
             ArrayList<String> exptokens = lisp.tokenize(expression);
             ArrayList<String> fibtokens = lisp.tokenize(fibonacci);
 
             // Evalúa la expresión tokenizada
-            System.out.println("Resultado de farenheit a celsius: " + lisp.eval(exptokens));
-            System.out.println("Resultado de fibonacci: " + lisp.eval(fibtokens));
+            System.out.println("Resultado de la condición: " + lisp.eval(condtokens));
+            //System.out.println("Resultado de farenheit a celsius: " + lisp.eval(exptokens));
+            //System.out.println("Resultado de fibonacci: " + lisp.eval(fibtokens));
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
