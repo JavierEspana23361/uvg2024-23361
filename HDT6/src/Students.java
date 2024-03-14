fcde82c47c365a7f6c606be043644c5d7f15ee8e
JavierEspana23361 <esp23361@uvg.edu.gt>
2024-03-14 11:58:44 -0600

    aaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- HDT6/src/Students.java
+++ HDT6/src/Students.java
@@ -1,4 +1,33 @@
 import java.util.HashMap;
+import java.util.Scanner;
+
+
 public class Students {
-    
+    public Object mapType(){
+        Scanner sc = new Scanner(System.in);
+        fileReader fr = new fileReader();
+        System.out.println("Selecciona el tipo de mapa que deseas utilizar: ");
+        System.out.println("1. HashMap");
+        System.out.println("2. TreeMap");
+        System.out.println("3. LinkedHashMap");
+        int option = sc.nextInt();
+        sc.close();
+        switch(option){
+            case 1:
+                HashMap<String, String> map = new HashMap<>();
+                map = fr.readtoHasMap("estudiantes.json");
+                return null;
+            case 2:
+                fr.readtoTreeMap("estudiantes.json");
+                return null;
+            case 3:
+                fr.readtoLinkedHashMap("estudiantes.json");
+                return null;
+            default:
+                System.out.println("Opción no válida");
+                return null;
+                
+        }
+        
+    }
 }
--- HDT6/src/fileReader.java
+++ HDT6/src/fileReader.java
@@ -1,21 +1,58 @@
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.BufferedReader;
+import java.util.LinkedHashMap;
+import com.fasterxml.jackson.core.type.TypeReference;
+import java.util.TreeMap;
 
 public class fileReader {
-    public Arraylist<String> leerEstudiantesDesdeJSON(String rutaArchivo) {
-        ArrayList<String> estudiantes = new ArrayList<>();
-        ObjectMapper objectMapper = new ObjectMapper();
-
-        try (FileReader reader = new FileReader(rutaArchivo)) {
-            Student[] estudiantesArray = objectMapper.readValue(reader, Student[].class);
-            estudiantes.addAll(Arrays.asList(estudiantesArray));
+    public Map<String, String> readtoHasMap(String rutaArchivo) {
+        Map<String, String> map = new HashMap<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(rutaArchivo))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split(":");
+                if (parts.length == 2) {
+                    String key = parts[0].trim();
+                    String value = parts[1].trim();
+                    map.put(key, value);
+                }
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return map;
+    }
 
-        return estudiantes;
+    public Map<String, String> readtoTreeMap(String rutaArchivo) {
+        Map<String, String> map = new TreeMap<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(rutaArchivo))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split(":");
+                if (parts.length == 2) {
+                    String key = parts[0].trim();
+                    String value = parts[1].trim();
+                    map.put(key, value);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return map;
+    }
+
+    public Map<String, String> readtoLinkedHashMap(String rutaArchivo) {
+        Map<String, String> map = new LinkedHashMap<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(rutaArchivo))) {
+            ObjectMapper objectMapper = new ObjectMapper();
+            map = objectMapper.readValue(br, new TypeReference<LinkedHashMap<String, String>>() {});
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return map;
     }
 }
