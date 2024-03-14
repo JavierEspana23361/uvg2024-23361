33a8b7d5892cc70cb0b5dea825d17290a91869b4
JavierEspana23361 <esp23361@uvg.edu.gt>
2024-03-14 11:31:50 -0600

    avances

--- HDT6/src/hashMD5.java
+++ HDT6/src/hashMD5.java
@@ -1,10 +1,24 @@
 // Crear las clases para la implementación de la función Hash Organica, MD5 y SHA-1.
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class hashMD5{
     
 
-    public String md5(String data) {
-        return data;
+    public String md5(String input) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] hashBytes = md.digest(input.getBytes());
+            StringBuilder sb = new StringBuilder();
+            for (byte b : hashBytes) {
+                sb.append(String.format("%02x", b));
+            }
+            return sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
 }
--- HDT6/src/hashSHA1.java
+++ HDT6/src/hashSHA1.java
@@ -1,5 +1,19 @@
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class hashSHA1 {
     public String sha1(String data) {
-        return data;
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-1");
+            byte[] hashBytes = md.digest(data.getBytes());
+            StringBuilder sb = new StringBuilder();
+            for (byte b : hashBytes) {
+                sb.append(String.format("%02x", b));
+            }
+            return sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
