5c3c65e6e72727dafc1b64e160200c3c0b0dccda
AngelEsquit <esq23221@uvg.edu.gt>
2024-03-14 11:43:47 -0600

    Update hashSHA1.java

--- HDT6/src/hashSHA1.java
+++ HDT6/src/hashSHA1.java
@@ -2,10 +2,10 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class hashSHA1 {
-    public String sha1(String data) {
+    public String sha1(String input) {
         try {
             MessageDigest md = MessageDigest.getInstance("SHA-1");
-            byte[] hashBytes = md.digest(data.getBytes());
+            byte[] hashBytes = md.digest(input.getBytes());
             StringBuilder sb = new StringBuilder();
             for (byte b : hashBytes) {
                 sb.append(String.format("%02x", b));
