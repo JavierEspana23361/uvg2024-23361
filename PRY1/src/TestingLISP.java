1e223552e33368b85224df86bd3b809446e6ba09
AndreEstrada1x <est23659@uvg.edu.gt>
2024-03-18 17:49:24 -0600

    aaa?

--- PRY1/src/TestingLISP.java
+++ PRY1/src/TestingLISP.java
@@ -1,40 +1,66 @@
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
-
+import static org.junit.Assert.*;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 public class TestingLISP {
-    @Test
-    public void testTokenize() {
-        LispInterpreter interpreter = new LispInterpreter();
 
-        String expression1 = "(+ 2 3)";
-        ArrayList<Object> expression = interpreter.tokenize(expression1);
-        ArrayList<String> expected1 = new ArrayList<String>();
-        expected1.add("(");
-        expected1.add("+");
-        expected1.add("2");
-        expected1.add("3");
-        expected1.add(")");
-        assertEquals(expected1, expression);
+    @Test
+    public void testArithmeticOperations() throws Exception {
+        LispInterpreter interpreter = new LispInterpreter();
+        
+        // Test addition
+        ArrayList<String> additionExpr = interpreter.tokenize("(+ 2 3)");
+        assertEquals(5.0, interpreter.eval(additionExpr));
+
+        // Test subtraction
+        ArrayList<String> subtractionExpr = interpreter.tokenize("(- 5 3)");
+        assertEquals(2.0, interpreter.eval(subtractionExpr));
+
+        // Test multiplication
+        ArrayList<String> multiplicationExpr = interpreter.tokenize("(* 2 3)");
+        assertEquals(6.0, interpreter.eval(multiplicationExpr));
+
+        // Test division
+        ArrayList<String> divisionExpr = interpreter.tokenize("(/ 6 2)");
+        assertEquals(3.0, interpreter.eval(divisionExpr));
     }
 
     @Test
-    public void testEval() throws Exception {
+    public void testVariableAssignmentAndEvaluation() throws Exception {
         LispInterpreter interpreter = new LispInterpreter();
 
-        String expression1 = "(+ 2 3)";
-        ArrayList<Object> expression = interpreter.tokenize(expression1);
-        Double expected1 = 5.0;
-        
-        // Crear un mapa de variables locales vacío
-        Map<String, Object> localVariables = new HashMap<>();
-        
-        // Llamar al método eval con la lista de elementos y el mapa de variables locales
-        Object result1 = interpreter.eval(expression, localVariables);
-        assertEquals(expected1, result1);
+        // Assign variable x
+        ArrayList<String> setqExpr = interpreter.tokenize("(SETQ x 5)");
+        assertEquals("Variable set: x = 5.0", interpreter.eval(setqExpr));
+
+        // Evaluate expression using variable x
+        ArrayList<String> evalExpr = interpreter.tokenize("(* x 2)");
+        assertEquals(10.0, interpreter.eval(evalExpr));
     }
 
+    @Test
+    public void testConditionalExpression() throws Exception {
+        LispInterpreter interpreter = new LispInterpreter();
+
+        // Test true condition
+        ArrayList<String> trueCondExpr = interpreter.tokenize("(COND (T 1) (NIL 2))");
+        assertEquals(1.0, interpreter.eval(trueCondExpr));
+
+        // Test false condition
+        ArrayList<String> falseCondExpr = interpreter.tokenize("(COND (NIL 1) (T 2))");
+        assertEquals(2.0, interpreter.eval(falseCondExpr));
+    }
+
+    @Test
+    public void testFunctionDefinitionAndEvaluation() throws Exception {
+        LispInterpreter interpreter = new LispInterpreter();
+
+        // Define a function
+        ArrayList<String> defunExpr = interpreter.tokenize("(DEFUN double (x) (* x 2))");
+        interpreter.eval(defunExpr);
+
+        // Test function call
+        ArrayList<String> functionCallExpr = interpreter.tokenize("(double 3)");
+        assertEquals(6.0, interpreter.eval(functionCallExpr));
+    }
 }
