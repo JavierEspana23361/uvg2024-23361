9ec24a08c2908e137ebdb23c769d900ca896ad94
AndreEstrada1x <est23659@uvg.edu.gt>
2024-03-19 00:27:21 -0600

    Interpreter1.0

--- PRY1/src/GUI.java
+++ PRY1/src/GUI.java
@@ -15,18 +15,20 @@ public class GUI extends JFrame {
         interpreter = new LispInterpreter();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        // Barra lateral
         JPanel panelPrincipal = new JPanel(new BorderLayout());
         int margenWidth = 250;
         JPanel margenPanel = new JPanel(new BorderLayout());
         margenPanel.setPreferredSize(new Dimension(margenWidth, 600));
         margenPanel.setBackground(new Color(52, 73, 94));
 
-        // Título elegante
+        // Título
         JLabel titleLabel = new JLabel("Lisp Interpreter", SwingConstants.CENTER);
         titleLabel.setForeground(Color.WHITE);
         titleLabel.setFont(new Font("Georgia", Font.BOLD, 24));
         margenPanel.add(titleLabel, BorderLayout.CENTER);
 
+        // Entrada y salida
         JPanel panel = new JPanel(new BorderLayout());
 
         entradaTextField = new JTextField();
@@ -34,12 +36,14 @@ public class GUI extends JFrame {
         historialTextArea = new JTextArea();
         historialTextArea.setEditable(false);
 
+        // Desplazamiento del historial
         JScrollPane scrollPane = new JScrollPane(historialTextArea);
         panel.add(entradaTextField, BorderLayout.SOUTH);
         panel.add(scrollPane, BorderLayout.CENTER);
 
         panelPrincipal.add(panel, BorderLayout.CENTER);
 
+        // Imagen
         JPanel imagePanel = new JPanel();
         imagePanel.setLayout(new BoxLayout(imagePanel, BoxLayout.Y_AXIS)); 
         JLabel imageLabel = new JLabel();
@@ -53,6 +57,7 @@ public class GUI extends JFrame {
             ex.printStackTrace();
         }
 
+        // Añadir paneles
         panelPrincipal.add(margenPanel, BorderLayout.WEST);
         add(panelPrincipal);
 
@@ -63,6 +68,7 @@ public class GUI extends JFrame {
         entradaTextField.requestFocusInWindow();
     }
 
+    // Ejecutar comando
     private void ejecutarComando(String comando) {
         ArrayList<String> tokens = interpreter.tokenize(comando);
         actualizarHistorial("Entrada: " + comando);
@@ -75,6 +81,7 @@ public class GUI extends JFrame {
         entradaTextField.setText("");
     }
 
+    // Actualizar historial
     private void actualizarHistorial(String texto) {
         historialTextArea.append(texto + "\n");
     }
