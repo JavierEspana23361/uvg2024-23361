34b9f1366f640b49137580cdf81d06bdea744cb3
JavierEspana23361 <esp23361@uvg.edu.gt>
2024-03-18 19:25:36 -0600

    main arreglado

--- PRY1/src/Main.java
+++ PRY1/src/Main.java
@@ -1,24 +1,7 @@
-import java.util.ArrayList;
-import java.util.Scanner;
+import javax.swing.SwingUtilities;
 
 public class Main {
     public static void main(String[] args) {
-        // Entrada para probar el interprete con definir funciones defun: (DEFUN factorial (n) (COND ((EQUAL n 0) 1) (T (* n (factorial (- n 1))))))
-        // Entrada para probar el interprete con ejecutar funciones defun: (factorial 5)
-        Scanner sc = new Scanner(System.in);
-        LispInterpreter lisp = new LispInterpreter();
-        
-        while (true) {
-            System.out.println("< ");
-            String operation = sc.nextLine();
-            try {
-                ArrayList<String> tokens = lisp.tokenize(operation);
-                Object result = lisp.eval(tokens);
-                System.out.println("Resultado: " + result);
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-            }
-            System.out.println("> ");
-        }
+        SwingUtilities.invokeLater(GUI::new);
     }
 }
