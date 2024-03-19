869ed7090aff67cde6f8bd71315186425d2e2175
AndreEstrada1x <est23659@uvg.edu.gt>
2024-03-19 00:09:12 -0600

    Lisp Interpreter

--- PRY1/src/GUI.java
+++ PRY1/src/GUI.java
@@ -19,7 +19,7 @@ public class GUI extends JFrame {
         int margenWidth = 250;
         JPanel margenPanel = new JPanel(new BorderLayout());
         margenPanel.setPreferredSize(new Dimension(margenWidth, 600));
-        margenPanel.setBackground(new Color(52, 73, 94)); // Establecer color de fondo
+        margenPanel.setBackground(new Color(52, 73, 94));
 
         // Título elegante
         JLabel titleLabel = new JLabel("Lisp Interpreter", SwingConstants.CENTER);
@@ -41,20 +41,18 @@ public class GUI extends JFrame {
         panelPrincipal.add(panel, BorderLayout.CENTER);
 
         JPanel imagePanel = new JPanel();
-        imagePanel.setLayout(new BoxLayout(imagePanel, BoxLayout.Y_AXIS)); // Cambiar a BoxLayout con eje vertical
+        imagePanel.setLayout(new BoxLayout(imagePanel, BoxLayout.Y_AXIS)); 
         JLabel imageLabel = new JLabel();
         try {
             Image image = ImageIO.read(new File("resources/Logo.jpg"));
             ImageIcon imageIcon = new ImageIcon(image.getScaledInstance(margenWidth, -1, Image.SCALE_DEFAULT));
             imageLabel.setIcon(imageIcon);
             imagePanel.add(imageLabel);
-            // Agregar panel de imagen al panel del margen
             margenPanel.add(imagePanel, BorderLayout.NORTH);
         } catch (IOException ex) {
             ex.printStackTrace();
         }
 
-        // Agregar margenPanel al panel principal
         panelPrincipal.add(margenPanel, BorderLayout.WEST);
         add(panelPrincipal);
 
