aef2f830ad034dadb56bff33d556a992eb1ad93a
JavierEspana23361 <esp23361@uvg.edu.gt>
2024-03-22 22:00:43 -0600

    AAAAAAAAAAAAAAA

--- HDT6/src/MapFactory.java
+++ HDT6/src/MapFactory.java
@@ -1,6 +1,5 @@
 import java.util.HashMap;
 import java.util.LinkedHashMap;
-import java.util.Map;
 import java.util.TreeMap;
 import java.util.AbstractMap;
 
--- HDT6/src/hashORGANIC.java
+++ HDT6/src/hashORGANIC.java
@@ -1,5 +1,9 @@
 public class hashORGANIC implements Ihash{
     public String typehash(String data) {
-        return data;
+        int hash = 0;
+        for (int i = 0; i < data.length(); i++) {
+            hash = (hash * 31) + data.charAt(i);
+        }
+        return Integer.toString(hash);
     }
 }
