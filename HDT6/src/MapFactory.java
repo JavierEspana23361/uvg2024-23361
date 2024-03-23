35a956553789fd7d5e3616c9408a7dd23cde4dab
AngelEsquit <esq23221@uvg.edu.gt>
2024-03-22 18:13:45 -0600

    Update MapFactory.java

--- HDT6/src/MapFactory.java
+++ HDT6/src/MapFactory.java
@@ -1,25 +1,23 @@
 import java.util.HashMap;
 import java.util.LinkedHashMap;
-import java.util.Map;
 import java.util.TreeMap;
+import java.util.AbstractMap;
 
-public class MapFactory {
-    public enum MapType {
-        HASHMAP,
-        TREEMAP,
-        LINKEDHASHMAP
-    }
+public class MapFactory<K, V> {
+    public static final int HASHMAP = 1;
+    public static final int TREEMAP = 2;
+    public static final int LINKEDHASHMAP = 3;
 
-    public static Map<String, String> createMap(MapType type) {
-        switch (type) {
+    public AbstractMap<K, V> getMapInstance(int option) {
+        switch (option) {
             case HASHMAP:
-                return new HashMap<>();
+                return new HashMap<K, V>();
             case TREEMAP:
-                return new TreeMap<>();
+                return new TreeMap<K, V>();
             case LINKEDHASHMAP:
-                return new LinkedHashMap<>();
+                return new LinkedHashMap<K, V>();
             default:
-                throw new IllegalArgumentException("Tipo de mapa no válido: " + type);
+                return new HashMap<K, V>();
         }
     }
 }
