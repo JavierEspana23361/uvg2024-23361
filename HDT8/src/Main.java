58ff1ec84987e80e3d96dae9c75556657eef3419
AngelEsquit <esq23221@uvg.edu.gt>
2024-04-11 11:36:00 -0600

    AAAAAAAAAAAAAAAA

--- HDT8/src/Main.java
+++ HDT8/src/Main.java
@@ -1,5 +1,48 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList; 
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }   
+        String filePathTxt = "procesos.txt";
+
+        ArrayList<ArrayList<String>> text = txtReader(filePathTxt);
+        
+        for (ArrayList<String> line : text) {
+            for (String word : line) {
+                System.out.println(word);
+            }
+        }
+    }
+    
+    public static String priority(ArrayList<ArrayList<String>> text) {
+        String translatedText = "";
+        for (ArrayList<String> line : text) {
+            for (String word : line) {
+                
+            }
+            translatedText += "\n";
+        }
+        return translatedText;
+    }
+
+    public static ArrayList<ArrayList<String>> txtReader(String filePath) {
+        ArrayList<ArrayList<String>> linesArrayList = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] linesList = line.split(",");
+                ArrayList<String> lines = new ArrayList<>();
+                for (int i = 0; i < linesList.length; i++) {
+                    lines.add(linesList[i]);
+                }
+                linesArrayList.add(lines);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return linesArrayList;
+    }
 }
