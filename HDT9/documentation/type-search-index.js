68bbac0b3c19c34a690f6ff8d8eae9fb26154260
JavierEspana23361 <esp23361@uvg.edu.gt>
2024-05-07 23:19:24 -0600

    Docs

---
+++ HDT9/documentation/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Huffman"},{"p":"<Unnamed>","l":"Leaf"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"Node"}];updateSearchResults();
