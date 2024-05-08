7f7ccb60bc6ecd6758192d6af943952f15f35543
AngelEsquit <esq23221@uvg.edu.gt>
2024-05-07 20:31:30 -0600

    Create Leaf.java

---
+++ HDT9/src/Leaf.java
@@ -0,0 +1,12 @@
+public class Leaf extends Node {
+    private final char character;
+
+    public Leaf(char character, int frequency) {
+        super(frequency);
+        this.character = character;
+    }
+
+    public char getCharacter() {
+        return character;
+    }
+}
