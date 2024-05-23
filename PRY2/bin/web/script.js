2cccbdbbc676e7edda0be29e20b16223ba7662b2
JavierEspana23361 <esp23361@uvg.edu.gt>
2024-05-22 18:17:33 -0600

    sas

---
+++ PRY2/bin/web/script.js
@@ -0,0 +1,66 @@
+function toggleLoginForm() {
+    var loginForm = document.getElementById("loginForm");
+    if (loginForm.style.display === "none" || loginForm.style.display === "") {
+        loginForm.style.display = "block";
+        document.getElementById("welcome").style.display = "none";
+    } else {
+        loginForm.style.display = "none";
+    }
+}
+
+function login() {
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    fetch('/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username: username, password: password }),
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.name) {
+            document.getElementById("usernameDisplay").textContent = data.name;
+            document.getElementById("loginForm").style.display = "none";
+            document.getElementById("welcome").style.display = "block";
+        } else {
+            alert("Usuario o contraseña incorrectos");
+        }
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+}
+
+function showRecommendations() {
+    fetch('/recommendations')
+    .then(response => response.json())
+    .then(data => {
+        let recommendationsDiv = document.getElementById("recommendations");
+        recommendationsDiv.innerHTML = "<h3>Series recomendadas:</h3>";
+        data.forEach(serie => {
+            recommendationsDiv.innerHTML += `<p>${serie}</p>`;
+        });
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+}
+
+function insertSeries() {
+    // Lógica para insertar una serie
+}
+
+function connectSeriesToGenre() {
+    // Lógica para conectar una serie a un género
+}
+
+function addSeriesToUser() {
+    // Lógica para añadir series a un usuario
+}
+
+function addGenreToUser() {
+    // Lógica para añadir un género a un usuario
+}
